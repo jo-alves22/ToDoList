57233c2cb999ca2f18a63b96feac6f154d8aac2f
jo-alves22 <jonasadrianoalves@gmail.com>
2023-10-12 16:37:46 -0300

    first commit

---
+++ src/main/java/br/com/jonasalves/todolist/user/UserModel.java
@@ -0,0 +1,11 @@
+package br.com.jonasalves.todolist.user;
+
+public class UserModel {
+    
+    public String username;
+    public String name;
+    public String password;
+    /*
+     * 
+     */
+}
