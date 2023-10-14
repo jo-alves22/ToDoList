79462287457500965b28f27a0622815fdca06f91
jo-alves22 <jonasadrianoalves@gmail.com>
2023-10-14 14:28:08 -0300

    add validation and update tasks

--- src/main/java/br/com/jonasalves/todolist/user/UserController.java
+++ src/main/java/br/com/jonasalves/todolist/user/UserController.java
@@ -1,5 +1,6 @@
 package br.com.jonasalves.todolist.user;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
 
 /**
  * Modificador
@@ -29,6 +32,11 @@ public class UserController {
             System.out.println("Usuário já existe");
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
         }
+
+        var passwordHashred = BCrypt.withDefaults().hashToString(12, userModel.getPassword().toCharArray());
+
+        userModel.setPassword(passwordHashred);
+
         var userCreated = this.userRepository.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
